04c7a510e4dde7b563e0d15a97eac6ed17d52fe6
Pranav0913 <161944546+Pranav0913@users.noreply.github.com>
2024-03-02 18:14:14 +0530

    Add files via upload

---
+++ arithmatic_operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter the value of A and B"<<endl;
+	cin>>a>>b;
+	cout<<"A+B="<<a+b<<endl;
+	cout<<"A-B="<<a-b<<endl;
+	cout<<"A*B="<<a*b<<endl;
+	cout<<"A/B="<<a/b<<endl;
+	cout<<"A%B="<<a            %b<<endl;
+	return 0;
+}
