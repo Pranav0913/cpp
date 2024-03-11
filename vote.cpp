755c905c417929db5d2cc4dd41f5eb7fd98893dd
Pranav0913 <161944546+Pranav0913@users.noreply.github.com>
2024-03-11 12:13:08 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout<<"enter the age"<<endl;
+	cin>>i;
+	if(i>18)
+	{
+		cout<<"you can vote"<<endl;
+	}
+		else
+		cout<<"you cannot vote"<<endl;
+	
+	return 0;
+}
