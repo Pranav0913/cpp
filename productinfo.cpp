1a80246d17c4dc0951a2399370166c9de7238138
Pranav0913 <161944546+Pranav0913@users.noreply.github.com>
2024-05-02 02:58:38 -0700

    Add files via upload

---
+++ productinfo.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class product
+{
+	private:
+		string name;
+		float price;
+		int quantity;
+		
+		public:
+			product(string productname,float productprice,int productquantity)
+			{
+				name=productname;
+				price=productprice;
+				quantity=productquantity;
+			}
+			string getname()const
+			{
+				return name;
+			}
+			float getprice()const
+			{
+				return price;
+			}
+			int getquantity() const
+			{
+				return quantity;
+			}
+			
+			void setname(string newname)
+			{
+				name=newname;
+			}
+			void setprice(float newprice)
+			{
+				price=newprice;
+			}
+			void setquantity(int newquantity)
+			{
+				quantity=newquantity;
+			}
+			void displayinfo() const
+			{
+				cout<<"product name:"<<name<<endl;
+				cout<<"product price:"<<price<<endl;
+				cout<<"product quantity:"<<quantity<<endl;
+			}
+};
+
+int main()
+{
+	product product1("mobile",50000,5);
+	product1.displayinfo();
+	return 0;
+}
